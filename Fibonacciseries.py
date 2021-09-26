ff8ff6d200803f5aa237b1fe644ed020d65d4efe
Padmasrry <91425889+Padmasrry@users.noreply.github.com>
2021-09-26 21:40:10 +0530

    Create Fibonacciseries.py

---
+++ Fibonacciseries.py
@@ -0,0 +1,19 @@
+#fibonacci series
+
+N = int(input("Fibonacci numbers in following integer sequence  : "))
+
+fibonacciSeries = [0,1]
+
+if N>2:
+
+    for i in range(2, N):
+
+        
+
+        nextElement = fibonacciSeries[i-1] + fibonacciSeries[i-2]
+
+        
+
+        fibonacciSeries.append(nextElement)
+
+print(fibonacciSeries)
