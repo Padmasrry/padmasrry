2f0443818dbe8534c4d77885e871160e4e0f9aa1
Padmasrry <91425889+Padmasrry@users.noreply.github.com>
2021-10-02 14:45:24 +0530

    Create openread.py

---
+++ openread.py
@@ -0,0 +1,24 @@
+#opening a file
+
+if=open("padma.txt","r")
+print(f.read())
+
+
+#reading a file
+
+f=open("padma.txt","r")
+print(f.readline())
+
+
+
+
+#open a file
+
+f=open("padma.txt","r")
+for x in f:
+  print(x)
+
+
+  
+
+    
